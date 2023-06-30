1ab1311dfa743fb4d893998ac8e41e05b167cb20
pisces2336 <pisces2336@gmail.com>
2023-06-30 23:25:03 +0900

    移動アニメーション実装

--- web/webapp-learn/app.vue
+++ web/webapp-learn/app.vue
@@ -10,23 +10,27 @@
 
     <hr>
 
-    <div class="row mt-3">
-      <div v-for="kanban_category in kanban_list" class="col-4 px-3">
-        <div v-for="k in kanban_category" class="card">
+    <div class="row mt-3 px-5 h-auto">
+      <div v-for="kanban_category in kanban_list" class="col-4">
+        <h2 v-if="kanban_list.indexOf(kanban_category) == 0">未着手: {{ kanban_category.length }}</h2>
+        <h2 v-else-if="kanban_list.indexOf(kanban_category) == 1">作業中: {{ kanban_category.length }}</h2>
+        <h2 v-else-if="kanban_list.indexOf(kanban_category) == 2">完了: {{ kanban_category.length }}</h2>
 
-          <div class="card-body">
-            <h5 class="card-title">{{ k.title }}</h5>
-            <p class="card-text">{{ k.body }}</p>
-            <div class="d-flex justify-content-between">
-              <button v-if="k.category >= 1" @click="moveKanban(k, 'left')" class="btn btn-dark">◀</button>
-              <button v-else class="btn btn-secondary" disabled>◀</button>
+        <TransitionGroup name="list" tag="ul" class="position-relative list-unstyled">
+          <li v-for="k in kanban_category" :key="k" class="card w-100 mx-auto mb-3">
+            <div class="card-body">
+              <h5 class="card-title">{{ k.title }}</h5>
+              <p class="card-text">{{ k.body }}</p>
+              <div class="d-flex justify-content-between">
+                <button v-if="k.category >= 1" @click="moveKanban(k, 'left')" class="btn btn-dark">◀</button>
+                <button v-else class="btn btn-secondary" disabled>◀</button>
 
-              <button v-if="k.category <= 1" @click="moveKanban(k, 'right')" class="btn btn-dark">▶</button>
-              <button v-else class="btn btn-secondary" disabled>▶</button>
+                <button v-if="k.category <= 1" @click="moveKanban(k, 'right')" class="btn btn-dark">▶</button>
+                <button v-else class="btn btn-secondary" disabled>▶</button>
+              </div>
             </div>
-          </div>
-
-        </div>
+          </li>
+        </TransitionGroup>
       </div>
     </div>
   </div>
@@ -74,3 +78,20 @@
     kanban_list.value[kanban.category].push(kanban)
   }
 </script>
+<style>
+  .list-move,
+  .list-enter-active,
+  .list-leave-active {
+    transition: all 0.5s ease;
+  }
+
+  .list-enter-from,
+  .list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+
+  .list-leave-active {
+    position: absolute;
+  }
+</style>
