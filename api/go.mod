332942231dc3d95df4c1b8fd68b7f1355bfae66d
pisces2336 <pisces2336@gmail.com>
2023-07-01 22:58:32 +0900

    dbへの接続を実装

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,10 @@
 module main
 
 go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/jinzhu/gorm v1.9.16
+)
+
+require github.com/jinzhu/inflection v1.0.0 // indirect
--- api/main.go
+++ api/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
 
 func main() {
-	fmt.Println("g")
+	_, err := sqlConnect()
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("db connection success!")
+	}
+}
+
+func sqlConnect() (*gorm.DB, error) {
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		panic(err)
+	}
+	c := mysql.Config{
+		DBName:    "webapp_learn",
+		User:      "root",
+		Passwd:    os.Getenv("MYSQL_ROOT_PASSWORD"),
+		Addr:      "db:3306",
+		Net:       "tcp",
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+
+	fmt.Println(c.FormatDSN())
+
+	return gorm.Open("mysql", c.FormatDSN())
 }
